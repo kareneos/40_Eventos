6338378e488c1d8414b0832fc1b17495bb2416ac
Karen os <karenestelaos@gmail.com>
2016-10-24 16:47:34 -0500

    versiones

--- Trello/js/main.js
+++ Trello/js/main.js
@@ -1,49 +1,50 @@
+//crea input nombre de la lista
+var input = document.createElement("input");
+input.setAttribute("placeholder","Ingrese nombre de la lista");
+input.setAttribute("id","inputTituloLista");
+input.setAttribute("class", "mt-5 ml-3");
+//crea botón guardar
+var boton2 = document.createElement("button");
+boton2.setAttribute("class","btn btn-primary btn-sm");
+//crear texto del boton guardar
+var textoBoton2 = document.createTextNode("Guardar");
+//Agregar texto a guardar
+boton2.appendChild(textoBoton2);
+//div para añarir input y boton gardar
+var boton1 = document.getElementById("anadeLista");
+//añadir input y botón guardar al hacer click en botón1
 function muestraInput(){
-    //crear input
-    var input = document.createElement("input");
-    input.setAttribute("placeholder","Ingrese nombre de la lista");
-    input.setAttribute("id","inputTituloLista");
-    input.setAttribute("class", "mt-5 ml-3");
-    //crear botón2
-    var boton2 = document.createElement("button");
-    boton2.setAttribute("class","btn btn-primary btn-sm");
-    //crear texto del botón2
-    var textoBoton2 = document.createTextNode("Guardar");
-    //Agregar texto a botón2
-    boton2.appendChild(textoBoton2);
-    //añadir input y botón2 al hacer click en botón1
-    var boton1 = document.getElementById("anadeLista");
     boton1.appendChild(input);
     boton1.appendChild(boton2);
     input.focus();
-    //crear título al dar click en guardar
-    boton2.onclick = function(){
-        if (input.value.length == 0){
-            alert("Ingrese nombre de la lista");
-        }else{
-            //ocultar ingrese nombre de la lista
-            boton1.style.display="none";
-            //crea elemento
-            var div = document.createElement("div");
-            var textoTitulo = input.value;
-            var titulo = document.createTextNode(textoTitulo);
-            div.appendChild(titulo);
-            var titulo1 = document.getElementById("titulo");
-            titulo1.appendChild(div);
-           //Deja input en blanco
-            document.getElementById("inputTituloLista").value = "";
-            //Botón añadir una tarjeta
-            var anadirTarjeta = document.createElement("button");
-            anadirTarjeta.setAttribute("class","btn btn-success btn-sm");
-            var textoBotonTarjeta = document.createTextNode("Añadir una tarjeta...");
-            anadirTarjeta.appendChild(textoBotonTarjeta);
-            titulo1.appendChild(anadirTarjeta);
-            //Textarea
-            anadirTarjeta.onclick = function(){
-                var textArea = document.createElement("textarea");
-                div.appendChild(textArea);
-                textArea.focus();
-            }
+}
+//crear título al dar click en guardar
+boton2.onclick = function(){
+    if (input.value.length == 0){
+        alert("Ingrese nombre de la lista");
+    }else{
+        //ocultar ingrese nombre de la lista
+        boton1.style.display="none";
+        //crea elemento
+        var div = document.createElement("div");
+        var textoTitulo = input.value;
+        var titulo = document.createTextNode(textoTitulo);
+        div.appendChild(titulo);
+        var titulo1 = document.getElementById("titulo");
+        titulo1.appendChild(div);
+       //Deja input en blanco
+        document.getElementById("inputTituloLista").value = "";
+        //Botón añadir una tarjeta
+        var anadirTarjeta = document.createElement("button");
+        anadirTarjeta.setAttribute("class","btn btn-success btn-sm");
+        var textoBotonTarjeta = document.createTextNode("Añadir una tarjeta...");
+        anadirTarjeta.appendChild(textoBotonTarjeta);
+        titulo1.appendChild(anadirTarjeta);
+        //Textarea
+        anadirTarjeta.onclick = function(){
+            var textArea = document.createElement("textarea");
+            div.appendChild(textArea);
+            textArea.focus();
         }
     }
 }
